b0d5d03c04c2da890ca7b0a626b82d4e465b0ae6
Gosia <gosia.warwaszynska@gmail.com>
2021-11-30 12:32:17 +0100

    images-updated-working

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import image from "../beer-images/elhefe.png";
+// import image from "../beer-images/elhefe.png";
 export default function Product(props) {
   const [setMessage] = useState("hi mom");
 
@@ -15,11 +15,12 @@ export default function Product(props) {
         backgroundColor: color,
       }}
     >
-      {/* <img src={`./beer-images/${props.label}`} alt="Product" className="beerImage" /> */}
-      <img src={image} alt="img" className="beerImage" />
+      <img src={`../beer-images/${props.label}`} alt={props.label} className="beerImage" />
+      {/* <img src={image} alt="img" className="beerImage" /> */}
       <h2>{props.name}</h2>
       <div className="beer-price">
-        <h3>{props.category}|</h3>
+        <h3>{props.category}</h3>
+        <div className="line">|</div>
         <p>50,-</p>
       </div>
       <div className="addBasket">
