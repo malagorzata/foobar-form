bdc7ca53a24a32c3718eb2d4650f26c5db4bdae3
anasofich <castellanos_chopas@hotmail.com>
2021-11-30 12:09:56 +0100

    adjusted productview css

--- src/components/ProductView.js
+++ src/components/ProductView.js
@@ -11,16 +11,16 @@ export default function ProductView() {
             <span className="alcPer">5.4</span>% alc, <span>Germany</span>
           </div>
           <div className="qualities">
-            <div className="quality 1">HEFEWEIZEN</div>
+            <div className="quality 1">Hefeweizen</div>
             <div className="quality 2">light</div>
             <div className="quality 3">bitter</div>
           </div>
           <div className="productDesc">Refreshing wheat or rye beers that can display more hop character and less yeast character than their German cousins.</div>
           <div className="productVPrice">40dkk</div>
           <div className="singleProductButtons">
-            <button>-</button>
+            <button className="minusBtn">-</button>
             <p>2</p>
-            <button>+</button>
+            <button className="plusBtn">+</button>
             <button className="addBtn">add to cart</button>
           </div>
         </div>
