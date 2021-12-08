74d0ae8735693a46ac0d21643f2e38ab59573651
Gosia <gosia.warwaszynska@gmail.com>
2021-12-08 18:49:22 +0100

    small update

--- src/components/PaymentBasketItems.js
+++ src/components/PaymentBasketItems.js
@@ -13,61 +13,6 @@ export default function PaymentBasketItems() {
           </div>
           <p className="finalBeerPrice">80 DKK</p>
         </article>
-        <article className="fullOrder">
-          <img src="../beer-images/fairytaleale.png" alt="el hefe" className="checkoutImg" />
-          <div className="orderDetails">
-            <h2 className="beerNameOrder">Hoppily Ever After</h2>
-            <p className="beerPriceOrder">40 DKK</p>
-            <div className="qtCheckout">
-              <button className="minus"> - </button>3<button className="plus"> + </button>
-            </div>
-          </div>
-          <p className="finalBeerPrice">120 DKK</p>
-        </article>
-        <article className="fullOrder">
-          <img src="../beer-images/fairytaleale.png" alt="el hefe" className="checkoutImg" />
-          <div className="orderDetails">
-            <h2 className="beerNameOrder">Hoppily Ever After</h2>
-            <p className="beerPriceOrder">40 DKK</p>
-            <div className="qtCheckout">
-              <button className="minus"> - </button>3<button className="plus"> + </button>
-            </div>
-          </div>
-          <p className="finalBeerPrice">120 DKK</p>
-        </article>{" "}
-        <article className="fullOrder">
-          <img src="../beer-images/fairytaleale.png" alt="el hefe" className="checkoutImg" />
-          <div className="orderDetails">
-            <h2 className="beerNameOrder">Hoppily Ever After</h2>
-            <p className="beerPriceOrder">40 DKK</p>
-            <div className="qtCheckout">
-              <button className="minus"> - </button>3<button className="plus"> + </button>
-            </div>
-          </div>
-          <p className="finalBeerPrice">120 DKK</p>
-        </article>{" "}
-        <article className="fullOrder">
-          <img src="../beer-images/fairytaleale.png" alt="el hefe" className="checkoutImg" />
-          <div className="orderDetails">
-            <h2 className="beerNameOrder">Hoppily Ever After</h2>
-            <p className="beerPriceOrder">40 DKK</p>
-            <div className="qtCheckout">
-              <button className="minus"> - </button>3<button className="plus"> + </button>
-            </div>
-          </div>
-          <p className="finalBeerPrice">120 DKK</p>
-        </article>{" "}
-        <article className="fullOrder">
-          <img src="../beer-images/fairytaleale.png" alt="el hefe" className="checkoutImg" />
-          <div className="orderDetails">
-            <h2 className="beerNameOrder">Hoppily Ever After</h2>
-            <p className="beerPriceOrder">40 DKK</p>
-            <div className="qtCheckout">
-              <button className="minus"> - </button>3<button className="plus"> + </button>
-            </div>
-          </div>
-          <p className="finalBeerPrice">120 DKK</p>
-        </article>
       </div>
 
       <section id="finalOrderPrice">
